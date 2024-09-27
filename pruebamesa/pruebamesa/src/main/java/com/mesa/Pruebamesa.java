950f47e790b92b6cea0db5641cb5a3061bd60fdb
Cristhianmca <cristhianmc84@gmail.com>
2024-09-26 19:55:45 -0500

    añadiendo hola2

---
+++ pruebamesa/pruebamesa/src/main/java/com/mesa/Pruebamesa.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mesa;
+
+/**
+ *
+ * @author crist
+ */
+public class Pruebamesa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("hola");
+        System.out.println("hola1");
+        System.out.println("hola2");
+    }
+}
